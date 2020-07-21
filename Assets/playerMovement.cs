d72ef215077fc31558cd5d1abbc23609c73a79a6
liamtheriot <liamtheriot@outlook.com>
2020-07-21 17:29:28 -0500

    movement kinda added assets

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public charactercontroller controller;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 40f;
+
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.deltaTime, false, jump);
+        jump = false;
+    }
+}
